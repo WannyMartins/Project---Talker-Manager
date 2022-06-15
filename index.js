1d1afdb3f3c39f8c4607431dd5a5936ecd4b6788
Wanny Martins <wcatery@gmail.com>
2022-06-15 19:01:40 -0300

    crisndo rota que filtra apenas um id

--- index.js
+++ index.js
@@ -19,8 +19,21 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (_request, response) => {
   try {
     const talker = await talkersFs.getTalkers();
-    console.log(talker);
     return response.status(HTTP_OK_STATUS).json(talker);
+  } catch (e) {
+    return response.status(500).end();
+} 
+});
+
+app.get('/talker/:id', async (request, response) => {
+  try {
+    const talker = await talkersFs.getTalkers();
+    const { id } = request.params;
+    const talk = talker.find((r) => r.id === Number(id));
+    if (!talk) {
+      return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return response.status(HTTP_OK_STATUS).json(talk);
   } catch (e) {
     return response.status(500).end();
 } 
